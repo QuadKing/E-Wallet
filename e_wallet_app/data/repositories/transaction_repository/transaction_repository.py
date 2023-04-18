66f42243b6c3e8257e32f72b5321bf2c34a2ab7b
Moyinoluwa Michael <ogunsmoyin.m@gmail.com>
2023-04-18 13:59:34 +0100

    initial commit

---
+++ e_wallet_app/data/repositories/transaction_repository/transaction_repository.py
@@ -0,0 +1,16 @@
+from e_wallet_app.data.models.transaction import Transaction
+
+
+class TransactionRepository:
+
+    def save(self, transaction: Transaction) -> Transaction:
+        raise NotImplementedError
+
+    def count(self) -> int:
+        raise NotImplementedError
+
+    def find_by_id(self, id_num: int) -> Transaction:
+        raise NotImplementedError
+
+    def find_all_by_account_id(self, account_id_num: int) -> list[Transaction]:
+        raise NotImplementedError
