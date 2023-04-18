1234a0aefdf3237b29f23d2ac06eacabfe460048
unknown <marvebirthday7@gmail.com>
2023-04-18 18:36:22 +0100

    updated

--- test/test_account_service.py
+++ test/test_account_service.py
@@ -4,10 +4,6 @@ from e_wallet_app.data.models.account import Account
 from e_wallet_app.data.repositories.account_repository.account_repository import AccountRepository
 from e_wallet_app.data.repositories.account_repository.account_repository_impl import AccountRepositoryImpl
 from e_wallet_app.dtos.request.account_creation_request import AccountCreationRequest
-from e_wallet_app.services.account_service import AccountService
-from e_wallet_app.services.account_service_impli import AccountServiceImpl
-
-from e_wallet_app.dtos.request.account_creation_request import AccountCreationRequest
 from e_wallet_app.dtos.response.account_response import AccountResponse
 from e_wallet_app.exceptions.duplicate_account_exception import DuplicateAccountException
 from e_wallet_app.services.account_service import AccountService
@@ -35,10 +31,6 @@ class TestAccountService(TestCase):
         self.account_response = self.account_service.create_new_account(self.account_request)
 
     def test_account_can_be_find_by_id(self):
-        ##self.account = Account()
-       ## self.account_request = AccountCreationRequest()
-        ##self.account_service.create_new_account(self.account_request)
-
         self.assertEquals("Prof", self.account_service.find_account_by_id(1).get_first_name())
 
     def test_account_can_be_created(self):
